a8a4952fd986d0bd7ce5f3418b6375844493bf5e
raghu2671 <raghu.gadasu2017@gmail.com>
2024-12-28 16:19:31 +0530

    if conidtion testing with frontend

--- frontend.sh
+++ frontend.sh
@@ -5,22 +5,44 @@ component=frontend
 
 echo installing nginx
 dnf install nginx -y &>> $log_file
-echo $?
+
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
 
 echo copying the expese.conf to default.d
 cp expense.conf /etc/nginx/default.d/expense.conf &>> $log_file
-echo $?
+f [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 echo removing the old nginx web content
 rm -rf /usr/share/nginx/html/* &>> $log_file
-echo $?
+f [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 cd /usr/share/nginx/html
 
 download_and_extract
-echo $?
+f [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 echo starting nginx service
 systemctl enable nginx &>> $log_file
 systemctl restart nginx &>> $log_file
-echo $?
+f [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
