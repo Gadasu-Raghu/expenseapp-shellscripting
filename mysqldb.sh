a4a80ac4e4cce05910b6deb6491aed9a75a05969
raghu2671 <raghu.gadasu2017@gmail.com>
2024-12-28 20:50:10 +0530

    updating color code for forntend and backend

--- mysqldb.sh
+++ mysqldb.sh
@@ -3,22 +3,22 @@ source common.sh
 
 echo Disable mysql default version
 dnf module disable mysql -y &>>$log_file
-echo $?
+stat_check
 
 echo Copy Mysql repo file
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
-echo $?
+stat_check
 
 echo install mysql server
 dnf install mysql-community-server -y &>>$log_file
-echo $?
+stat_check
 
 echo start mysql service
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld &>>$log_file
-echo $?
+stat_check
 
 echo setup root password
 mysql_secure_installation --set-root-pass ExpenseApp@1  &>>$log_file
 mysql -uroot -p ExpenseApp@1 &>>$log_file
-echo $?
+stat_check
